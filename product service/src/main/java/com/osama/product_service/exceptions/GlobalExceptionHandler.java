7efd9df7b096885054a3133a55776e105d37c939
7roubb <7roubb@gmail.com>
2025-06-25 13:00:02 +0300

    Product Exception

--- product service/src/main/java/com/osama/product_service/exceptions/GlobalExceptionHandler.java
+++ product service/src/main/java/com/osama/product_service/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.osama.product_service.exceptions;
 import com.osama.product_service.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -26,13 +27,81 @@ public class GlobalExceptionHandler {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+
         ApiResponse<Map<String, String>> response = ApiResponse.<Map<String, String>>builder()
                 .status(HttpStatus.BAD_REQUEST)
-                .message("Invalid user data")
+                .message(getMessage(
+                        "validation.failed",
+                        new Object[]{ex.getMessage()})
+                )
                 .content(errors)
                 .build();
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(CustomExceptions.ProductNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleProductNotFound(CustomExceptions.ProductNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(getMessage(
+                        "product.not.found",
+                                new Object[]{ex.getMessage()})
+                        , HttpStatus.NOT_FOUND));
+    }
 
+    @ExceptionHandler(CustomExceptions.ProductAlreadyExistsException.class)
+    public ResponseEntity<ApiResponse<Void>> handleProductAlreadyExists(CustomExceptions.ProductAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ApiResponse.error(getMessage(
+                        "product.already.exists",
+                                new Object[]{ex.getMessage()})
+                        , HttpStatus.CONFLICT));
+    }
+
+    @ExceptionHandler(CustomExceptions.InvalidCategoryException.class)
+    public ResponseEntity<ApiResponse<Void>> handleInvalidCategory(CustomExceptions.InvalidCategoryException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(getMessage(
+                        "invalid.category",
+                                new Object[]{ex.getMessage()})
+                        , HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(CustomExceptions.OutOfStockException.class)
+    public ResponseEntity<ApiResponse<Void>> handleOutOfStock(CustomExceptions.OutOfStockException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(getMessage(
+                        "out.of.stock",
+                                new Object[]{ex.getMessage()})
+                        , HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(CustomExceptions.InvalidDiscountException.class)
+    public ResponseEntity<ApiResponse<Void>> handleInvalidDiscount(CustomExceptions.InvalidDiscountException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(getMessage(
+                        "invalid.discount",
+                                new Object[]{ex.getMessage()})
+                        , HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(CustomExceptions.ProductValidationException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationError(CustomExceptions.ProductValidationException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(ApiResponse.error(getMessage(
+                        "product.validation.failed",
+                                new Object[]{ex.getMessage()}),
+                        HttpStatus.UNPROCESSABLE_ENTITY));
+    }
+
+    @ExceptionHandler(CustomExceptions.InvalidLocalizedFieldException.class)
+    public ResponseEntity<ApiResponse<Void>> handleInvalidLocalizedField(CustomExceptions.InvalidLocalizedFieldException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(getMessage(
+                        "invalid.localized.field",
+                                new Object[]{ex.getMessage()})
+                        , HttpStatus.BAD_REQUEST));
+    }
+    private  String getMessage(String messageKey, Object[] args ) {
+        return messageSource.getMessage(messageKey, args, LocaleContextHolder.getLocale());
+    }
 
 }
