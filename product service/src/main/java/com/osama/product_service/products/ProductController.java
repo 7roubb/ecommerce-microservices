7ae7d9191785e87df0940d338510828bab58d38f
7roubb <7roubb@gmail.com>
2025-06-25 19:04:42 +0300

    Controller

---
+++ product service/src/main/java/com/osama/product_service/products/ProductController.java
@@ -0,0 +1,56 @@
+package com.osama.product_service.products;
+
+import com.osama.product_service.common.ApiResponse;
+import com.osama.product_service.common.OnCreate;
+import com.osama.product_service.common.OnUpdate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+    private final MessageSource messageSource;
+
+    @GetMapping
+    public ApiResponse<Page<ProductResponseDto>> getAllProducts(Pageable pageable) {
+        Page<ProductResponseDto> products = productService.findAll(pageable);
+        return ApiResponse.success(products, HttpStatus.OK, getMessage("product.get.all.success"));
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<ProductResponseDto> getProductById(@PathVariable String id) {
+        ProductResponseDto product = productService.getProductByID(id);
+        return ApiResponse.success(product, HttpStatus.OK, getMessage("product.get.success", id));
+    }
+
+    @PostMapping
+    public ApiResponse<Boolean> saveProduct(@Validated(OnCreate.class) @RequestBody ProductRequestDto productDto) {
+        Boolean result = productService.saveProduct(productDto);
+        return ApiResponse.success(result, HttpStatus.CREATED, getMessage("product.create.success", productDto.getName()));
+    }
+
+    @PutMapping
+    public ApiResponse<Boolean> updateProduct(@Validated(OnUpdate.class) @RequestBody ProductRequestDto productDto) {
+        Boolean result = productService.updateProduct(productDto);
+        return ApiResponse.success(result, HttpStatus.OK, getMessage("product.update.success", productDto.getId()));
+    }
+
+    @DeleteMapping
+    public ApiResponse<Boolean> deleteProduct(@RequestParam String id) {
+        Boolean result = productService.deleteProduct(id);
+        return ApiResponse.success(result, HttpStatus.OK, getMessage("product.delete.success", id));
+    }
+
+    private String getMessage(String code, String... args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+}
